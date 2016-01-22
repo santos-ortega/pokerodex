64e93265c645fbbf8a366eae99cbca187d5936ee
Santos Ortega <seortega13@gmail.com>
2016-01-21 22:42:06 -0800

    Initial Commit

---
+++ pokeroldex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokeroldex
+//
+//  Created by Santos Ortega on 1/21/16.
+//  Copyright © 2016 Santos Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
